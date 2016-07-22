7541a7215bb70b7691276607a7382d0886bdbbc4
全志安 <quanza@mlxing.com>
2016-07-22 15:25:05 +0800

    调整分页和添加异常处理

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ var router = express.Router();
 /* GET users listing. */
 router.get('/', function (req, res) {
 
-    console.log(req.query);
+    //console.log(req.query);
     // find
     var pagesize = req.query.pagesize || 20, pageno = req.query.pageno || 1;
     pagesize = parseInt(pagesize);
@@ -20,6 +20,12 @@ router.get('/', function (req, res) {
             list: data.rows,
             paginator: pagination(pageno, pagesize, data.rows.length, data.count, req.baseUrl)
         });
+    }).catch (function(err){
+        res.status(500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
 
 });
@@ -27,7 +33,7 @@ router.get('/', function (req, res) {
 /* GET users list listing. */
 router.get('/list', function (req, res) {
 
-    console.log(req.query);
+    //console.log(req.query);
     // find
     var pagesize = req.query.pagesize || 20, pageno = req.query.pageno || 1;
     pagesize = parseInt(pagesize);
@@ -40,6 +46,12 @@ router.get('/list', function (req, res) {
         res.render('users_list', {
             title: '用户列表',
             list: users
+        });
+    }).catch (function(err){
+        res.status(500);
+        res.render('error', {
+            message: err.message,
+            error: err
         });
     });
 
@@ -56,11 +68,19 @@ router.get('/create', function (req, res) {
  * post保存新增数据
  */
 router.post('/create', function (req, res) {
-    models.cms_admin_user.create({
-        username: req.body.username
-    }).then(function () {
+    //{title:req.body.title}
+    req.body.id = "UFX" + Math.round(100000);
+    //console.log(req.body);
+    models.cms_admin_user.create(req.body).then(function () {
         res.redirect('/users');
+    }).catch (function(err){
+        res.status(500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
+
 });
 
 /**
@@ -69,6 +89,12 @@ router.post('/create', function (req, res) {
 router.get('/:id/update', function (req, res) {
     models.cms_admin_user.findById(req.params.id).then(function (users) {
         res.render('users_edit', {title: '用户编辑', data: users});
+    }).catch (function(err){
+        res.status(500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
 });
 
@@ -76,11 +102,22 @@ router.get('/:id/update', function (req, res) {
  * 更新
  */
 router.post('/update', function (req, res) {
-    console.log(req.body);
-    models.cms_admin_user.update({
-        title: req.body.title
+    //console.log(req.body);
+    //{title:req.body.title}
+    var _id = req.body.id;
+    delete req.body.id;
+    models.cms_admin_user.update(req.body,{
+        'where':{
+            'id':_id
+        }
     }).then(function () {
-        res.redirect('/');
+        res.redirect('/users');
+    }).catch (function(err){
+        res.status(500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
 });
 
@@ -91,7 +128,13 @@ router.get('/:id/delete', function (req, res) {
     models.cms_admin_user.destroy({
         'where': {'id': req.params.id}
     }).then(function () {
-        res.redirect('/');
+        res.redirect('/users');
+    }).catch (function(err){
+        res.status(500);
+        res.render('error', {
+            message: err.message,
+            error: err
+        });
     });
 
 });
