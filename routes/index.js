d12bcf2537f94298074b8d17484ef7667e25250f
全志安 <quanza@mlxing.com>
2016-07-22 11:13:05 +0800

    添加分页和前端列表页面分页功能

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var models  = require('../models');
+var models = require('../models');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
 
     //数据
     var data = {
@@ -27,19 +27,19 @@ router.get('/', function (req, res, next) {
 });
 
 /* upload page. */
-router.post('/upload', function (req, res, next) {
-    for (var i in req.files) {
-        if (req.files[i].size == 0){
-            // 使用同步方式删除一个文件
-            fs.unlinkSync(req.files[i].path);
-            console.log('Successfully removed an empty file!');
-        } else {
-            var target_path = './public/images/' + req.files[i].name;
-            // 使用同步方式重命名一个文件
-            fs.renameSync(req.files[i].path, target_path);
-            console.log('Successfully renamed a file!');
-        }
-    }
+router.post('/upload', function (req, res) {
+    //for (var i in req.files) {
+    //    if (req.files[i].size == 0) {
+    //        // 使用同步方式删除一个文件
+    //        fs.unlinkSync(req.files[i].path);
+    //        console.log('Successfully removed an empty file!');
+    //    } else {
+    //        var target_path = './public/images/' + req.files[i].name;
+    //        // 使用同步方式重命名一个文件
+    //        fs.renameSync(req.files[i].path, target_path);
+    //        console.log('Successfully renamed a file!');
+    //    }
+    //}
     req.flash('success', '文件上传成功!');
     res.redirect('/upload');
 });
